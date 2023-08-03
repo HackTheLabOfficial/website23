ce186a9cdcf36208ac7216c3158423509d758405
Jiaming Situ <112342179+Jiaming-S@users.noreply.github.com>
2023-08-03 12:15:52 -0700

    fancy navbar

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
-
-const navList = document.querySelectorAll('.nav > * > *');
+const navList = document.querySelectorAll(".nav > * > *");
 
 const navMapping = {
-  'prizes-nav'  : '#prizes-section',
-  'sponsors-nav': '#sponsors-section',
-  'schedule-nav': '#schedule-section',
-  'team-nav'    : '#team-section',
-  'faq-nav'     : '#faq-section',
+  "prizes-nav": "#prizes-section",
+  "sponsors-nav": "#sponsors-section",
+  "schedule-nav": "#schedule-section",
+  "team-nav": "#team-section",
+  "faq-nav": "#faq-section",
 };
 
 navList.forEach((navItem) => {
-  navItem.addEventListener('click', (e) => {
+  navItem.addEventListener("click", (e) => {
     e.preventDefault();
-    const target = navMapping[navItem.getAttribute('id')];
-    document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
+    const target = navMapping[navItem.getAttribute("id")];
+    document.querySelector(target).scrollIntoView({ behavior: "smooth" });
   });
 });
 
+document.addEventListener("mousemove", updateCursorPosition);
+
+function updateCursorPosition(e) {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  document.documentElement.style.setProperty("--cursor-x", x + "px");
+  document.documentElement.style.setProperty("--cursor-y", y + "px");
+}
